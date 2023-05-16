141ed60e74d8e71fd8e4bb0453ad62e463616725
fezinho <opugnero@gmail.com>
2023-05-16 09:51:58 -0300

    movie detail

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,10 +5,10 @@ const MovieCard = ({ movieProp }) => {
   return (
     <div className="movie-card">
         <div className="movie-image">
-            <img src={`https://image.tmdb.org/t/p/w1280${movieProp.poster_path}`} alt={""} />
+            <img src={`https://image.tmdb.org/t/p/w1280${movieProp.poster_path}`} alt={movieProp.title} />
         </div>
         <div className="movie-description">
-            <h3>{""}</h3>
+            <h3>{movieProp.title}</h3>
           <Link to={`/movie/${movieProp.id}`} className="btn-details" >
             Ver Detalhes
           </Link>
--- src/views/MovieDetail.jsx
+++ src/views/MovieDetail.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Link, useParams } from "react-router-dom"
+import { MovieService } from "../api/MovieService"
 
 const MovieDetail = () => {
+  const {id} = useParams()
+  const [movie, setMovie] = useState({})
+
+  async function getMovie() {
+    const { data } = await MovieService.getMovieDetails(id)
+    setMovie(data)
+  }
+
+  useEffect(() => {
+    getMovie()
+  }, [])
+
   return (
-    <div>MovieDetail</div>
+    <section className="movie-detail">
+      <div className="movie-detail__container">
+        <div className="movie-detail__col">
+          <h1 className="movie-detail__title">{MovieDetail.title}</h1>
+          <div className="movie-detail__image">
+            <img src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`} alt="" />
+          </div>
+        </div>
+        <div className="movie-detail__col">
+          <div className="movie-detail__details">
+            <div className="movie-detail__detail">
+              <span>Budget:</span>{movie.budget}
+            </div>
+            <div className="movie-detail__detail">
+              <span>Original language:</span> {movie.original_language}
+            </div>
+            <div className="movie-detail__detail">
+              <span>Popularity:</span> {movie.popularity}
+            </div>
+            <div className="movie-detail__detail">
+              <span>Overview:</span> {movie.overview}
+            </div>
+          </div>
+          <Link to={"/"} className="movie-detail__button" >
+            Voltar
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
